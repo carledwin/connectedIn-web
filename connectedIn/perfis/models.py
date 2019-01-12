ddfaf8c83e96ea622d0381de7c900b31daa5f36b
Carl Edwin <carlinstr@gmail.com>
2019-01-12 21:49:47 -0200

    Configuração de model Pefil, makemigrations, migrate, Inclusão, alteração e consulta

--- connectedIn/perfis/models.py
+++ connectedIn/perfis/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class Perfil(object):
+#class Perfil(object):
 
-	def __init__(self, nome='', email='', telefone='', nome_empresa=''):
-		self.nome = nome
-		self.email = email
-		self.telefone = telefone
-		self.nome_empresa = nome_empresa
+# model.Model criara uma tabela no banco com colunas com nomes e tipos dos atributos
+class Perfil(models.Model):
+
+	#por nao herdar de object nao precisa do construtor abaixo
+	#def __init__(self, nome='', email='', telefone='', nome_empresa=''):
+	#	self.nome = nome
+	#	self.email = email
+	#	self.telefone = telefone
+	#	self.nome_empresa = nome_empresa
+
+	nome = models.CharField(max_length=255, null=False)
+	email = models.CharField(max_length=255, null=False)
+	telefone = models.CharField(max_length=15, null=False)
+	nome_empresa = models.CharField(max_length=255, null=False)
--- connectedIn/perfis/views.py
+++ connectedIn/perfis/views.py
@@ -11,12 +11,14 @@ def view_index(request):
 
 def view_perfil(request, perfil_id):
 
-	perfil = Perfil()
+	# perfil = Perfil()
 
-	if perfil_id == '1':
-		perfil = Perfil('Carl Edwin Antonio Nascimento', 'carl@email.com', '2222-4444', 'Alura')
+	#if perfil_id == '1':
+	#	perfil = Perfil('Carl Edwin Antonio Nascimento', 'carl@email.com', '2222-4444', 'Alura')
 
-	if perfil_id == '2':
-		perfil = Perfil('Albert', 'albert@email.com', '34334-6675', 'Sales')	
+	#if perfil_id == '2':
+	#	perfil = Perfil('Albert', 'albert@email.com', '34334-6675', 'Sales')	
+
+	perfil = Perfil.objects.get(id=perfil_id)
 
 	return render(request, 'perfil.html', {'perfil':perfil})
