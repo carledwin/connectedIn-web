34622bcfbf9095188bc80a4e44b93fdf7d9f509d
Carl Edwin <carlinstr@gmail.com>
2019-01-12 18:35:25 -0200

    Criação da view perfil, passagem de parâmetros, criação do model e exibição dinâmica

--- connectedIn/perfis/urls.py
+++ connectedIn/perfis/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
     # r'^$' --> raw string
     # url(r'^$', 'connectedIn.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'perfis.views.view_index'),
+    # ^comeca com 
+    # $ termina com
+    # \d+ com um ou mais digitos
+    #url(r'^perfis/\d+$', 'perfis.views.view_perfil'),
+    # grupo (?P<parm>\d+)
+    url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.view_perfil'),
+    
 )
--- connectedIn/perfis/views.py
+++ connectedIn/perfis/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from perfis.models import Perfil
 
 
 # Create your views here.
@@ -7,3 +8,15 @@ from django.shortcuts import render
 def view_index(request):
     #return HttpResponse("Bem vindo ao connectedIn")
     return render(request, 'index.html')
+
+def view_perfil(request, perfil_id):
+
+	perfil = Perfil()
+
+	if perfil_id == '1':
+		perfil = Perfil('Carl Edwin Antonio Nascimento', 'carl@email.com', '2222-4444', 'Alura')
+
+	if perfil_id == '2':
+		perfil = Perfil('Albert', 'albert@email.com', '34334-6675', 'Sales')	
+
+	return render(request, 'perfil.html', {'perfil':perfil})
